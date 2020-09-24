d70d631445a0fdbbc9b2f2cebec8a848d4ce5927
Sergey <sergstas0@gmail.com>
2020-09-24 11:58:37 +0500

    Measuring, algorithms implementation

---
+++ lab1/ACALab1/ACALab1/FirstPart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ACALab1
+{
+    public static class FirstPart
+    {
+        public static T Max<T>(IEnumerable<T> collection) where T : IComparable
+        {
+            try
+            {
+                var result = collection.First();
+                foreach (var e in collection)
+                    if (e.CompareTo(result) > 0)
+                        result = e;
+                return result;
+            }
+            catch { return default; }
+        }
+
+        public static int Count<T>(IEnumerable<T> collection)
+        {
+            var result = 0;
+            if (collection is null) 
+                return result;
+            foreach (var _ in collection)
+                result++;
+            return result;
+        }
+
+        public static void ArrayBubbleSort<T>(T[] collection) where T : IComparable
+        {
+            if (collection is null)
+                return;
+            for (var i = 0; i < collection.Length - 1; i++)
+            for (var j = i + 1; j < collection.Length; j++)
+                if (collection[i].CompareTo(collection[j]) > 0)
+                {
+                    var temp = collection[i];
+                    collection[i] = collection[j];
+                    collection[j] = temp;
+                }
+        }
+    }
+}
---
+++ lab1/ACALab1/ACALab1/Measurer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ACALab1
+{
+    public static class Measurer
+    {
+        public static double MeasureCollectionAlg<TEl, TRet>(Func<IEnumerable<TEl>, TRet> alg, IEnumerable<TEl> collection, int count)
+        {
+            var watch = new Stopwatch();
+            alg(collection);
+            watch.Start();
+            for (var i = 0; i < count; i++)
+                alg(collection);
+            watch.Stop();
+            return watch.ElapsedMilliseconds / 1000 / count;
+        }
+    }
+}
