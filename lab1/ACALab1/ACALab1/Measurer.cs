59a45c373ebb9a98f3933cf3152e7707d40ffd17
Sergey <sergstas0@gmail.com>
2020-09-27 13:18:19 +0500

    Refactored measurer

--- lab1/ACALab1/ACALab1/Measurer.cs
+++ lab1/ACALab1/ACALab1/Measurer.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace ACALab1
 {
     public static class Measurer
     {
-        public static double MeasureCollectionAlg<TEl, TRet>(Func<IEnumerable<TEl>, TRet> alg, IEnumerable<TEl> collection, int count)
+        public static double MeasureOnSpecifiedCollection<TEl, TRet>(Func<IEnumerable<TEl>, TRet> algorithm,
+            IEnumerable<TEl> collection)
+        {
+            algorithm(new TEl[0]);
+            return Measure(() => algorithm(collection)) / 1000;
+        }
+        
+        public static double MeasureOnRandomCollections<TEl, TRet>(Func<IEnumerable<TEl>, TRet> algorithm, 
+            Func<int, IEnumerable<TEl>> generator, int length, int iterations)
+        {
+            var collections = GenerateCollections(generator, length, iterations);
+            algorithm(generator(length));
+            return Measure(() =>
+                { foreach (var collection in collections) algorithm(collection); }) / 1000 / iterations;
+        }
+
+        private static TEl[][] GenerateCollections<TEl>(Func<int, IEnumerable<TEl>> generator, int length, int iterations)
+        /*  Сложность операции генерации коллекций - не меньше O(N)
+            время, затраченное на генерацию, в рассчет не идет        */ 
+        {
+            var result = new TEl[][iterations];
+            for (var i = 0; i < iterations; i++)
+                result[i] = generator(length).ToArray();
+            return result;
+        }
+
+        private static long Measure(Action act)
         {
             var watch = new Stopwatch();
-            alg(collection);
             watch.Start();
-            for (var i = 0; i < count; i++)
-                alg(collection);
+            act();
             watch.Stop();
-            return watch.ElapsedMilliseconds / 1000 / count;
+            return watch.ElapsedMilliseconds;
         }
     }
 }
