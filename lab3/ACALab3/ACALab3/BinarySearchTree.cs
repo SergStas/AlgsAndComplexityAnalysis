ceb7ea749e3daa974db30ee76d2be58a228af80d
Sergey <sergstas0@gmail.com>
2020-10-26 20:54:56 +0500

    Binary tree implementation has begun

---
+++ lab3/ACALab3/ACALab3/BinarySearchTree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ACALab3
+{
+    public class BinarySearchTree<TTree> where TTree : IComparable
+    {
+        private TreeNode<TTree> _root;
+
+        public void Add(TTree value)
+        {
+            var node = _root;
+            while (node?.DirectionOf(value) != null)
+                node = node.DirectionOf(value);
+            if (node == null)
+                _root = new TreeNode<TTree>(value);
+            else if (value.CompareTo(node.Value) <= 0)
+                node.AttachLeft(new TreeNode<TTree>(value));
+            else
+                node.AttachRight(new TreeNode<TTree>(value));
+        }
+
+        public bool Contains(TTree value)
+        {
+            var node = _root;
+            while (node != null)
+                if (node.Value.CompareTo(value) == 0)
+                    return true;
+                else
+                    node = node.DirectionOf(value);
+            return false;
+        }
+
+        public void Remove(TTree value)
+        {
+            if (!Contains(value))
+                return;
+            var node = _root;
+            while (node.Value.CompareTo(value) != 0)
+                node = node.DirectionOf(value);
+            if (node.Left == null && node.Right == null)
+                //node.
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        private class TreeNode<TNode> where TNode: IComparable
+        {
+            public TreeNode<TNode> Left { get; private set; }
+            public TreeNode<TNode> Right { get; private set; }
+            public TNode Value { get; }
+
+            public TreeNode(TNode value) => Value = value;
+
+            public void AttachLeft(TreeNode<TNode> value) => Left = value;
+            public void AttachRight(TreeNode<TNode> value) => Right = value;
+            public void DetachLeft() => Left = null;
+            public void DetachRight() => Right = null;
+            public TreeNode<TNode> DirectionOf(TNode value) => value.CompareTo(Value) <= 0 ? Left : Right;
+        }
+    }
+}
