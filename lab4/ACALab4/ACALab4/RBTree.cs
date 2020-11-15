7c285f5d9395d7996d728b6373552d3f80a4a5ef
Sergey <sergstas0@gmail.com>
2020-11-15 14:56:45 +0500

    Implemented add operation

--- lab4/ACALab4/ACALab4/RBTree.cs
+++ lab4/ACALab4/ACALab4/RBTree.cs
@@ -1,14 +1,22 @@
-﻿using System.Runtime.InteropServices.WindowsRuntime;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace ACALab4
 {
     public class RBTree
     {
         private Node _root;
-
+        
         public void Add(int key)
         {
-            
+            var node = _root;
+            while (node != null && !node.DirectionOf(key).IsNil)
+                node = node.DirectionOf(key);
+            var result = new Node(key, node) {IsBlack = _root is null};
+            if (node == null)
+                _root = result;
+            else
+                node.InsertChild(result);
+            result.Balance();
         }
 
         public Node Find(int key)
@@ -52,23 +60,6 @@ namespace ACALab4
             return node?.Key;
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         public class Node
         {
             public int Key { get; set; }
@@ -78,17 +69,65 @@ namespace ACALab4
             public bool IsLeft { get; set; }
             public Node Left { get; set; }
             public Node Right { get; set; }
-            
 
-            public Node Uncle => Parent is null ? null : Parent.IsLeft ? Parent.Parent?.Right : Parent.Parent?.Left;
+            private Node Uncle => Parent is null ? null : Parent.IsLeft ? Parent.Parent?.Right : Parent.Parent?.Left;
             
-            public Node(int key, Node parent, bool isLeft)
+            public Node(int key, Node parent)
             {
                 Key = key;
                 Parent = parent;
-                Left = new Node(default, this, true) {IsNil = true, IsBlack = true};
-                Right = new Node(default, this, false) {IsNil = true, IsBlack = true};
-                IsLeft = isLeft;
+                Left = new Node(this) {IsLeft = true};
+                Right = new Node(this);
+                IsLeft = key <= Parent?.Key;
+            }
+
+            private Node(Node parent)
+            {
+                IsNil = IsBlack = true;
+                Parent = parent;
+            }
+
+            public Node DirectionOf(int key) => key <= Key ? Left : Right;
+
+            public void InsertChild(Node node)
+            {
+                if (node.Key <= Key)
+                    Left = node;
+                else
+                    Right = node;
+                node.IsLeft = node.Key <= Key;
+            }
+
+            public void Balance()
+            {
+                if (Parent?.Parent is null || IsBlack)
+                    return;
+                if (!Uncle.IsBlack)
+                {
+                    Parent.IsBlack = Uncle.IsBlack = true;
+                    Parent.Parent.IsBlack = false;
+                    Parent.Parent.Balance();
+                    return;
+                }
+                if (IsLeft != Parent.IsLeft)
+                {
+                    var g = Parent.Parent;
+                    g.InsertChild(this);
+                    InsertChild(Parent);
+                    Parent.Parent = this;
+                    Parent = g;
+                }
+                Rotate(IsLeft);
+            }
+
+            private void Rotate(bool right)
+            {
+                var g = Parent.Parent;
+                var p = Parent;
+                g.InsertChild(right ? p.Right : p.Left);
+                p.InsertChild(g);
+                g.IsBlack = false;
+                p.IsBlack = true;
             }
         }
     }
