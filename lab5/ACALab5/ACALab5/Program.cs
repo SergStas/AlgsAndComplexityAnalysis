7c75d6b60e81897dff7426e4470ffb317c9be4f6
Sergey <sergstas0@gmail.com>
2020-11-28 14:13:45 +0500

    LAB5: text processor

--- lab5/ACALab5/ACALab5/Program.cs
+++ lab5/ACALab5/ACALab5/Program.cs
@@ -6,9 +6,7 @@ namespace ACALab5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var e = new[] {"s", "as", "", "b", ""};
-            StringSorter.QuickSort(e);
+            Console.ReadLine();
         }
     }
 }
---
+++ lab5/ACALab5/ACALab5/TextProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ACALab5
+{
+    public class TextProcessor
+    {
+        private readonly List<string> _tokens = new List<string>();
+        private static readonly char[] Splitters = {' ', ',', '.', ':', '-', '!', '?', ';', '\"', '(', ')', '#', '[', ']', '\t', '\n'};
+
+        public IEnumerable<string> ReadTokens(string path)
+        {
+            var content = File.ReadAllText(path);
+            foreach (var e in content.Split(Splitters).Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => s.ToLower()))
+                yield return e;
+        }
+
+        public void AddToDictionary(string path)
+        {
+            var content = File.ReadAllText(path);
+            foreach (var e in content.Split(Splitters).Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => s.ToLower()).Distinct())
+                if (!_tokens.Contains(e))
+                    _tokens.Add(e);
+        }
+
+        public string GenerateText(int wordsCount)
+        {
+            var builder = new StringBuilder();
+            var rnd = new Random();
+            for (var i = 0; i < wordsCount; i++)
+                builder.Append(_tokens[rnd.Next(0, _tokens.Count)] + (i == wordsCount - 1 ? "" : " "));
+            return builder.ToString();
+        }
+    }
+}
